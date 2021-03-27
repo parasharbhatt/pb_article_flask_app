34dd53ce3dbc6dda5c386a17f411717698024be3
parasharbhatt <parasharbhatt@yahoo.com>
2021-03-27 13:38:09 -0400

    modified app.py to begin coding

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 
-"""
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/home", method=["GET","POST"])
+@app.route("/home", methods=["GET","POST"])
 def home():
     return render_template("home.html")
     
-"""
 
-print("hello world")
+
+#print("hello world")
