164f1591306299c5384b08973b68d65ee79bc731
parasharbhatt <parasharbhatt@yahoo.com>
2021-03-28 00:00:54 -0400

    UI being developed

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-
+import datetime as dt
 
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+x=dt.datetime.now()
+sdt=x.strftime("%d-%b-%Y %H:%M:%S")
+#print(f"hello world today is : {sdt}")
 
+app = Flask(__name__)
+entries=[]
+
+@app.route("/", methods=["GET","POST"])
 @app.route("/home", methods=["GET","POST"])
 def home():
-    return render_template("home.html")
+    #print("calling Home route")
+    if request.method == "POST":
+        content_dict={}
+        for key, val in request.form.items():
+            if key == "title":
+                content_dict[key] = val
+            elif key == "content":
+                content_dict[key] = val
+                #request.form.get(key)
+        entries.append(content_dict)    
+                
+        #print(entries)
+    return render_template("home.html" , entries=entries)
     
 
 
-#print("hello world")
